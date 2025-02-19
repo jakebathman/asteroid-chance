a30a282ae8c06492676dbcdc8ecc0ffa7da75f27
Jake Bathman <jake.bathman@gmail.com>
2025-02-19 16:19:41 -0600

    Add hits

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Models\Hit;
 use App\Models\SpaceObject;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    // Register a webpage hit, just to track numbers (storing nothing)
+    Hit::create();
+
     $asteroid = SpaceObject::find(Cache::get('highest_risk'));
 
     return view('welcome', [
