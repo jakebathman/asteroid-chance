7b91e8ff7e13dcf98be924bf488f1db85ba0d532
Jake Bathman <jake.bathman@gmail.com>
2025-02-22 20:46:22 -0600

    Bump up main model exposure slightly
    It looks too dark on my phone :(

--- resources/views/components/model-viewer.blade.php
+++ resources/views/components/model-viewer.blade.php
@@ -1,5 +1,5 @@
 <model-viewer
-    {{ $attributes->merge(['shadow-intensity' => '0.5', 'exposure' => '0.04']) }}
+    {{ $attributes->merge(['shadow-intensity' => '0.5', 'exposure' => '0.08']) }}
     alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
     src="/Asteroid (46376) 2001 XD3.gltf"
     poster="Asteroid (46376) 2001 XD3.webp"
